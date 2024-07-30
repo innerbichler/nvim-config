60ddfca4a68c4fc1da3e74a1eb0823b88faa5bd2
alex wsl <alexander.innerbichler@bernard-gruppe.com>
2024-07-30 09:50:44 +0200

    added java formater on current file with <leader>jf

--- lua/alex/remap.lua
+++ lua/alex/remap.lua
@@ -1,4 +1,11 @@
 vim.g.mapleader = ' '
 vim.api.nvim_set_keymap('n', '<leader>pf', ':Ex<CR>', {})
 
+local function JavaGoogleFormat()
+	local current_file = vim.fn.expand('%:p')
+	vim.cmd('!java -jar ~/.config/nvim/google-java-format-1.22.0-all-deps.jar -i ' .. current_file)
+end
+vim.api.nvim_create_user_command('JavaGoogleFormat', JavaGoogleFormat, {})
+vim.api.nvim_set_keymap('n', '<leader>jf', ':JavaGoogleFormat<CR>', { noremap = true, silent = true })
+
 
